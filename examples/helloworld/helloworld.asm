a9edeb1ff894bf73eaa8811357f8457fd9f4a071
sfauvel <sfauvel@gmail.com>
2020-10-02 17:15:36 +0200

    End of kata

--- examples/helloworld/helloworld.asm
+++ examples/helloworld/helloworld.asm
@@ -5,8 +5,52 @@
         
         ; Define methods exported
         global  hello_world     ; Say hello
+        global  to_roman
         
         section .text
+to_roman:
+        mov rsi, output
+
+        ; Write to rax
+        cmp rdi, 1
+        jne not_I
+        call _I
+not_I:
+        cmp rdi, 5
+        je _V
+        cmp rdi, 10
+        je _X
+        cmp rdi, 2
+        je _II
+
+to_roman_after_cmp:
+
+        ; Add end character to string
+        mov [rsi], rax
+        add rsi, rbx
+        mov rax, 0
+        mov [rsi], rax
+        
+        mov rax, output
+        ret
+
+_I:
+        mov rax, [I]
+        mov rbx, 1
+        ret
+_V:
+        mov rax, [V]
+        mov rbx, 1
+        jmp to_roman_after_cmp 
+_X:
+        mov rax, [X]
+        mov rbx, 1
+        jmp to_roman_after_cmp
+_II:
+        mov rax, [II]
+        mov rbx, 2
+        jmp to_roman_after_cmp 
+
 hello_world:   
         mov rsi, output
 
@@ -25,6 +69,10 @@ hello_world:
         section   .data   
 hello:               db      'Hello'            ; String hello
 world:               db      'World'            ; String world
+I:                   db      'I'
+V:                   db      'V'
+X:                   db      'X'
+II:                  db      'II'
 
         section   .bss        
 output: resb         8*6                     ; Store score to build
--- examples/helloworld/helloworld.test.c
+++ examples/helloworld/helloworld.test.c
@@ -10,9 +10,26 @@
 #include <test.h>
 
 char* hello_world();
+char* to_roman();
 
-TEST void say_hello() {
+TEST void test_say_hello() {
     _assertStringEq("Hello", hello_world());
+}
+
+TEST void test_1_is_I() {
+    _assertStringEq("I", to_roman(1));
+}
+
+TEST void test_5_is_V() {
+    _assertStringEq("V", to_roman(5));
+}
+
+TEST void test_10_is_X() {
+    _assertStringEq("X", to_roman(10));
+}
+
+TEST void test_2_is_II() {
+    _assertStringEq("II", to_roman(2));
 }
 
 RUN_TESTS()
