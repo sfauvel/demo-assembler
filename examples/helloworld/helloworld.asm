c6fa462da97c32fbf4ec5f6533ec169b746994b5
sfauvel <sfauvel@gmail.com>
2020-11-07 16:23:15 +0100

    Two parameters

--- examples/helloworld/helloworld.asm
+++ examples/helloworld/helloworld.asm
@@ -7,17 +7,16 @@
         global  next_state_for     ;
         
         section .text
-next_state_for:        
-        mov rax, rsi
-        cmp rsi, 'O'
+next_state_for:                
+        cmp rdi, 'O'
         jne is_dead
 
 is_alive:
         mov rax, 'O'
         ret
-        
+
 is_dead:
-        cmp rdi, 3
+        cmp rsi, 3
         jne die
 alive:
         mov rax, 'O'
