bd4b47669f1ff46e4a126de7cde5cf8f27e4ebc3
sfauvel <sfauvel@gmail.com>
2021-04-16 18:27:48 +0200

    Clean folder

--- examples/helloworld/run.sh
+++ examples/helloworld/run.sh
@@ -1,6 +1,17 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
-pushd ${current_dir}/../..
-. run_tests.sh "*" examples/helloworld examples/helloworld
-popd
+nasm -felf64 asmunit.test.asm -o target/asmunit.test.o
+
+gcc target/asmunit.test.o -o  target/asmunit.test
+
+./target/asmunit.test
+
+nb_fails=$?
+
+echo
+if [ $nb_fails = 0 ]
+then
+    echo -e "\e[32mSuccess\e[0m"
+else
+    echo -e "\e[31mFailure\e[0m"
+fi
