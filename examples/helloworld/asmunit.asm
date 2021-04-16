4b4e50148c6e68097a7f5a25a558c63bebacb2b8
sfauvel <sfauvel@gmail.com>
2021-04-16 18:19:27 +0200

    Move display result to framework file

--- examples/helloworld/asmunit.asm
+++ examples/helloworld/asmunit.asm
@@ -4,13 +4,36 @@
 assert_equals:
     cmp rdi, rsi
     jne ae_fail
-   
+
     ae_success:
         mov rax, 0
         jmp end_assert_equals
 
     ae_fail:
         mov rax, 1
-        
+
     end_assert_equals:
         ret
+
+display_result:
+    cmp       rax, 1
+    je set_fail_message
+
+    set_success_message:
+        mov       rsi, message_success    ; address of string to output
+        jmp       output_message
+
+    set_fail_message:
+        mov       rsi, message_failure    ; address of string to output
+        jmp       output_message
+
+    output_message:
+        mov       rax, 1                  ; system call for write
+        mov       rdi, 1                  ; file handle 1 is stdout
+        mov       rdx, 1                 ; number of bytes
+        syscall                           ; invoke operating system to do the write
+        ret
+
+    section   .data
+message_success:  db        "."      ; note the newline at the end
+message_failure:  db        "X"      ; note the newline at the end
--- examples/helloworld/asmunit.test.asm
+++ examples/helloworld/asmunit.test.asm
@@ -24,9 +24,8 @@ test_assert_equals_fails_on_different_values:
     call assert_equals
     mov rdi, rax                ; assert_equals return  value
 
-    mov rsi, 1                  ; expected value: 1 = false   
-    call assert_equals    
-    
+    mov rsi, 1                  ; expected value: 1 = false
+    call assert_equals
     ret
 
 test_assert_equals_success_on_same_values:
@@ -36,31 +35,6 @@ test_assert_equals_success_on_same_values:
     mov rdi, rax                ; assert_equals return  value
 
     mov rsi, 0                  ; expected value: 0 = true
-    call assert_equals  
+    call assert_equals
     ret
 
-display_result:   
-    cmp       rax, 1
-    je display_fail
-    jmp display_success
-
-    display_fail:
-        mov       rax, 1                  ; system call for write
-        mov       rdi, 1                  ; file handle 1 is stdout
-        mov       rsi, message_failure    ; address of string to output
-        mov       rdx, 1                 ; number of bytes
-        syscall                           ; invoke operating system to do the write
-        ret 
-    
-    display_success:
-        mov       rax, 1                  ; system call for write
-        mov       rdi, 1                  ; file handle 1 is stdout
-        mov       rsi, message_success    ; address of string to output
-        mov       rdx, 1                 ; number of bytes
-        syscall                           ; invoke operating system to do the write
-        ret 
-
-
-    section   .data
-message_success:  db        "."      ; note the newline at the end
-message_failure:  db        "X"      ; note the newline at the end
