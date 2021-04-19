9f8567c8cff40f122788b601133cf390c6f56f26
sfauvel <sfauvel@gmail.com>
2021-04-19 23:33:46 +0200

    Move run_all_tests to framework

--- examples/helloworld/asmunit.asm
+++ examples/helloworld/asmunit.asm
@@ -1,5 +1,5 @@
 
-    
+
     section .text
 
 assert_equals:
@@ -15,6 +15,47 @@ assert_equals:
         mov [one_test_result], rax
 
     assert_equals_end:
+        ret
+
+run_all_tests:
+    push rbp
+    mov rbp, rsp
+
+    mov rax, [rbp+16+8*0]
+    push rax
+
+    .run_next_test:
+        pop rax
+        mov [nb_tests_to_run], rax
+        cmp rax, 0
+        jle .after_all_tests
+
+        call before_each
+
+    .next_test_address:
+        mov rax, [nb_tests_to_run]
+        mov dx, 8
+        mul dx
+
+        add rax, rbp
+        add rax, 16
+
+        mov rax, [rax]
+
+    .execute_next_test:
+
+        call rax
+        call after_each
+
+        mov rax, [nb_tests_to_run]
+        dec rax
+        push rax
+
+        jmp .run_next_test
+
+    .after_all_tests:
+        call after_all
+        leave
         ret
 
 before_all:
@@ -35,7 +76,7 @@ after_each:
     mov rax, [one_test_result]
     add [all_tests_result], rax
     mov rdi, [one_test_result]
-    call display_result      
+    call display_result
     ret
 
 
@@ -64,3 +105,4 @@ message_success:  db        "."      ; note the newline at the end
 message_failure:  db        "X"      ; note the newline at the end
 all_tests_result: dq         8       ; global result of execution
 one_test_result:  dq         8       ; result of one test
+nb_tests_to_run:  dq         0       ; nb tests to run
--- examples/helloworld/asmunit.test.asm
+++ examples/helloworld/asmunit.test.asm
@@ -7,7 +7,7 @@
 main:
     call before_all
 
-    
+
     push test_assert_equals_fails_on_different_values
     push test_assert_equals_success_on_same_values
     push test_result_is_fail_when_one_assert_fails
@@ -15,65 +15,22 @@ main:
 
     call run_all_tests
 
-    .remove_test_list_from_stack
-        pop rax
-        mov rdi, 8
-        mul rdi
-        add rsp, rax 
- 
+   .remove_test_list_from_stack
+       pop rax
+       mov rdi, 8
+       mul rdi
+       add rsp, rax
+
     mov rax, [all_tests_result]
     ret
-
-run_all_tests:
-    push rbp           
-    mov rbp, rsp  
-
-    
-    ;pop rax    
-    mov rax, [rbp+16+8*0]
-    push rax
-    
-    .run_next_test
-        pop rax
-        mov [nb_tests_to_run], rax
-        cmp rax, 0
-        jle .after_all_tests
-
-        call before_each
-
-    .next_test_address
-        mov rax, [nb_tests_to_run] 
-        mov dx, 8
-        mul dx
-
-        add rax, rbp
-        add rax, 16
-        
-        mov rax, [rax]
-
-    .execute_next_test    
-
-        call rax
-        call after_each
-
-        mov rax, [nb_tests_to_run]
-        dec rax
-        push rax
-
-        jmp .run_next_test
-
-    .after_all_tests:
-        call after_all    
-        leave
-        ret
 
 test_assert_equals_fails_on_different_values:
     mov rdi, 2
     mov rsi, 3
     call assert_equals
     mov [result], rax                ; assert_equals return  value
-    
-    call before_each 
+
+    call before_each
     mov rdi, [result]
     mov rsi, 1                  ; expected value: 1 = false
     call assert_equals
@@ -84,24 +41,24 @@ test_assert_equals_success_on_same_values:
     mov rsi, 2
     call assert_equals
     mov [result], rax                ; assert_equals return  value
-    
-    call before_each    
+
+    call before_each
     mov rdi, [result]
     mov rsi, 0                  ; expected value: 0 = true
     call assert_equals
     ret
 
 test_result_is_fail_when_one_assert_fails:
-    
+
     mov rdi, 2
     mov rsi, 3
     call assert_equals
-    
+
     mov rdi, 2
     mov rsi, 2
     call assert_equals
 
-    mov rdi, [one_test_result]  ; assert_equals return  value    
+    mov rdi, [one_test_result]  ; assert_equals return  value
     mov rsi, 1                  ; expected value: 0 = true
     call before_each
     call assert_equals
@@ -114,4 +71,3 @@ fail:
 
     section   .data
 result:  dq        8
-nb_tests_to_run:  dq         0       ; nb tests to run
