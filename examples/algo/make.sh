8b822282669a3afc40a2b0b4f92b761946da3e62
sfauvel <sfauvel@gmail.com>
2023-11-28 00:08:26 +0100

    Simplify make from examples

--- examples/algo/make.sh
+++ examples/algo/make.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-export FILE="algo"
-export ASM_PATH=examples/algo
-export TEST_PATH=examples/algo
-
 ../../scripts/make.sh $*
--- examples/tennis/make.sh
+++ examples/tennis/make.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-export FILE="tennis"
-export ASM_PATH=examples/tennis
-export TEST_PATH=examples/tennis
-
 ../../scripts/make.sh $*
