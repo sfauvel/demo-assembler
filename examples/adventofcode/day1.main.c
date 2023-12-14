2f47b3a64ddc25ce324323c60af9e9c0b9d41c3d
sfauvel <sfauvel@gmail.com>
2023-12-14 09:20:41 +0100

    Add performance measure

--- examples/adventofcode/day1.main.c
+++ examples/adventofcode/day1.main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <string.h>
+#include <time.h>
+#include <sys/time.h>
 
 #include <stdlib.h>
 #include <fcntl.h>
@@ -89,12 +91,55 @@ void compute_perf_from_file() {
     }
 }
 
+#define CYCLES_PER_SEC(ghz)     ((ghz) * 1e9)
+#define CYCLES_PER_MSEC(ghz)    ((ghz) * 1e6)
+#define CYCLES_PER_USEC(ghz)    ((ghz) * 1e3)
+#define GHZ 2.8 // Frequency of my computer : `cat /proc/cpuinfo | grep Hz`
+
+void iterate_to_compute_average_time_from_c(const char* filename) {
+    const unsigned long  NB_ITERATION_MAX = 200;
+    unsigned long nb_iteration=0;
+    time_t start_clock = clock();
+    struct timeval stop_time, start_time;
+    gettimeofday(&start_time, NULL);
+
+    // Clock => number of ticks of a process (pause are not counted)
+    // Time => real time
+
+    time_t t;   // not a primitive datatype
+    while (1) {   
+        nb_iteration++;
+  
+        calibration_from_file(filename);
+        
+        if (nb_iteration >= NB_ITERATION_MAX) { 
+            time_t end_clock = clock();
+            time(&t);
+            gettimeofday(&stop_time, NULL);
+    
+            unsigned long duration_time =(stop_time.tv_sec - start_time.tv_sec) * 1000000 + (stop_time.tv_usec - start_time.tv_usec);
+            unsigned long duration_clock = end_clock - start_clock;
+
+            //  printf("%ld %s\n", end-start, ctime(&t));
+            printf("%.0f clocks, clock:%.2fms, time:%.2fms, (%ld iterations - %.3fs)\n", (double)duration_clock/(double)nb_iteration*CYCLES_PER_USEC(GHZ), (double)duration_clock/(double)nb_iteration/1000.0, (double)duration_time/(double)nb_iteration/1000.0, nb_iteration, (double)duration_time / 1e6);
+           
+            nb_iteration = 0;
+            start_clock = end_clock;
+            start_time = stop_time;
+        }
+   }
+
+}
+
+
 int main(int argc, char **argv) {
     //compute_example("1abc3");
     compute_example("abc");
 
-    compute_perf_from_buffer();
-    compute_perf_from_file();
+    //compute_perf_from_buffer();
+    //compute_perf_from_file();
+     //iterate_to_compute_average_time_from_c("../examples/adventofcode/input_short.txt");
+    iterate_to_compute_average_time_from_c("../examples/adventofcode/input.txt");
 
     //compute_from_file();
 }
