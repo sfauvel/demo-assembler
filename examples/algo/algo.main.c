495b20e163d543caccfaf9ca1c91deadbef6d5cb
sfauvel <sfauvel@gmail.com>
2024-02-02 19:16:52 +0100

    Add a program to run game of life

--- examples/algo/algo.main.c
+++ examples/algo/algo.main.c
@@ -6,10 +6,12 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int add_5(int);
 
+int next_generation(int /* neighbor */, int /* state*/);
+char* board();
+void set_alive(int, int);
 
-int main(int argc, char **argv) {
-    int result = add_5(7);
-    printf("Result:%d\n", result);
+int main(int argc, char **argv) {   
+    set_alive(1, 1);
+    printf("%s", board());
 }
