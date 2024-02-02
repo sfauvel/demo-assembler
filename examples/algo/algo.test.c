fb5f69603f23de256fb3fbeb844de3629cf0395a
sfauvel <sfauvel@gmail.com>
2024-02-02 17:19:08 +0100

    Add some cases

--- examples/algo/algo.test.c
+++ examples/algo/algo.test.c
@@ -21,15 +21,20 @@ int next_generation(int /* neighbor */, int /* state*/);
 
 TEST void test_cell_with_more_then_3_neighbor_is_dead() {
     _assertIntEq(DEAD, next_generation(4, DEAD));
+    _assertIntEq(DEAD, next_generation(4, ALIVE));
 }
 
 TEST void test_dead_cell_or_alive_cell_with_3_neighbor_is_alive() {
     _assertIntEq(ALIVE, next_generation(3, DEAD));
+    _assertIntEq(ALIVE, next_generation(3, ALIVE));
 }
 
 TEST void test_cell_with_less_than_2_neighbor_is_dead() {
     _assertIntEq(DEAD, next_generation(1, DEAD));
     _assertIntEq(DEAD, next_generation(0, DEAD));
+
+    _assertIntEq(DEAD, next_generation(1, ALIVE));
+    _assertIntEq(DEAD, next_generation(0, ALIVE));
 }
 
 TEST void test_dead_cell_with_2_neighbor_is_dead() {
