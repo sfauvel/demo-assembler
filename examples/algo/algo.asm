e07971903a129ec6e743cf8e442e231247b49ad5
sfauvel <sfauvel@gmail.com>
2024-02-02 17:17:28 +0100

    Rule with 2 neighbors

--- examples/algo/algo.asm
+++ examples/algo/algo.asm
@@ -8,16 +8,24 @@
         global  next_generation     ;
 
         section .text
+; 1-RDI: neighbor
+; 2-RSI: state
 next_generation:
         cmp rdi, 4
         jge .die
         cmp rdi, 2
         jl .die
+        cmp rdi, 2
+        je .twoNeighbors
         mov rax, 1
         ret
 
         .die: 
                 mov rax, 0
+                ret
+        
+        .twoNeighbors:
+                mov rax, rsi
                 ret
 
 
--- examples/algo/algo.test.c
+++ examples/algo/algo.test.c
@@ -14,23 +14,30 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-
-int next_generation(int);
+int next_generation(int /* neighbor */, int /* state*/);
 
 #define DEAD 0
 #define ALIVE 1
 
 TEST void test_cell_with_more_then_3_neighbor_is_dead() {
-    _assertIntEq(DEAD, next_generation(4));
+    _assertIntEq(DEAD, next_generation(4, DEAD));
 }
 
-TEST void test_cell_with_3_neighbor_is_alive() {
-    _assertIntEq(ALIVE, next_generation(3));
+TEST void test_dead_cell_or_alive_cell_with_3_neighbor_is_alive() {
+    _assertIntEq(ALIVE, next_generation(3, DEAD));
 }
 
 TEST void test_cell_with_less_than_2_neighbor_is_dead() {
-    _assertIntEq(DEAD, next_generation(1));
-    _assertIntEq(DEAD, next_generation(0));
+    _assertIntEq(DEAD, next_generation(1, DEAD));
+    _assertIntEq(DEAD, next_generation(0, DEAD));
+}
+
+TEST void test_dead_cell_with_2_neighbor_is_dead() {
+    _assertIntEq(DEAD, next_generation(2, DEAD));
+}
+
+TEST void test_alive_cell_with_2_neighbor_is_alive() {
+    _assertIntEq(ALIVE, next_generation(2, ALIVE));
 }
 
 
