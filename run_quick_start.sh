17037169758fe3f65c15a6ed5f8098acbdf9fbd2
Sébastien Fauvel <sfauvel@gmail.com>
2026-02-18 23:35:50 +0100

    Improve quick_start script

--- run_quick_start.sh
+++ run_quick_start.sh
@@ -1,26 +1,29 @@
 #!/usr/bin/env bash
 
+GREY="\033[0;30m"
+GREEN="\033[0;32m"
+WHITE="\033[0;37m"
+NO_COLOR="\033[0m"
+
+function execute() {
+    local label=$1
+    local cmd="${@:2}"
+    echo -e "\n===  ${WHITE}${label}${NO_COLOR}  ===\n   ${GREEN}${cmd}${NO_COLOR}"
+    eval $cmd
+}
+
 function compileAndRunTest() {
 
-    test_name=$1
-    asm_path=$2
-    test_path=$3
+    local test_name=$1
+    local asm_path=$2
+    local test_path=$3
 
-    # Compile asm
-    local nasm_cmd="nasm -felf64 ${asm_path}/${test_name}.asm -o target/${test_name}.asm.o"
-    echo $nasm_cmd
-    eval $nasm_cmd
+    execute "Compile asm" nasm -felf64 ${asm_path}/${test_name}.asm -o target/${test_name}.asm.o
   
-    # Compile test
-    include_path=./test
-    gcc_cmd="gcc ${test_path}/${test_name}.test.c -I${include_path} target/${test_name}.asm.o -o target/${test_name}.test.o"
-    echo $gcc_cmd
-    eval $gcc_cmd
-    
-    echo "=================="
-    echo "Run ${test_name}.test"
-    echo "------------------"
-    ./target/${test_name}.test.o
+    local include_path=./test
+    execute "Compile test" gcc ${test_path}/${test_name}.test.c -I${include_path} target/${test_name}.asm.o -o target/${test_name}.test.o
+
+    execute "Launch ${test_name}.test" ./target/${test_name}.test.o
 }
 
 function clean() {
